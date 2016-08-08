9049aa7ee83d1438e2bd98196b3feb03ee03d3bc
Adi2016 <enriqe_spada@yahoo.com>
2016-08-08 17:10:35 +0300

    new site

---
+++ js/google.map.config.js
@@ -0,0 +1,45 @@
+(function($){
+	$(document).ready(function(){
+
+		// Google Maps
+		//-----------------------------------------------
+		if ($("#map-canvas").length>0) {
+			var map, myLatlng, myZoom, marker;
+			// Set the coordinates of your location
+			myLatlng = new google.maps.LatLng(44.431155, 26.131052);
+			myZoom = 16;
+			function initialize() {
+				var mapOptions = {
+					zoom: myZoom,
+					mapTypeId: google.maps.MapTypeId.ROADMAP,
+					center: myLatlng,
+					scrollwheel: true,
+					
+				};
+
+				
+				
+				map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+				marker = new google.maps.Marker({
+					map:map,
+					draggable: false,
+					animation: google.maps.Animation.DROP,
+					position: myLatlng
+
+				});
+				
+
+				google.maps.event.addDomListener(window, "resize", function() {
+					map.setCenter(myLatlng);
+					
+				});
+				
+				map.setOptions({draggable: false, zoomControl: false, scrollwheel: false, disableDoubleClickZoom: true});
+
+				
+			}
+			google.maps.event.addDomListener(window, "load", initialize);
+		}
+	}); // End document ready
+
+})(this.jQuery);		
