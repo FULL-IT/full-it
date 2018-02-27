8ad0339cac51445c8d5aac02638a1e680507e41a
Adrian1982 <adriantudora1303@gmail.com>
2018-02-27 20:36:08 +0200

    new build

--- js/google.map.config.js
+++ js/google.map.config.js
@@ -6,7 +6,7 @@
 		if ($("#map-canvas").length>0) {
 			var map, myLatlng, myZoom, marker;
 			// Set the coordinates of your location
-			myLatlng = new google.maps.LatLng(44.431155, 26.131052);
+			myLatlng = new google.maps.LatLng(44.431846, 26.033127);
 			myZoom = 16;
 			function initialize() {
 				var mapOptions = {
